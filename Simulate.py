02c242a7c498d5f11a642cb36721a845a17ad502
mr-0wl <craig@craighole.com>
2017-08-10 21:59:45 -0400

    sounds
    basic

---
+++ Simulate.py
@@ -0,0 +1,9 @@
+# Simulate is a Simon clone
+# From Invent with Python
+#A learning thing for working into game development
+# No copyright
+
+import random, sys, time, pygame
+from pygame.locals import *
+
+FPS = 30
