811f8c9477927bdfa567ff5bd64a94f7b2003d2c
mr-0wl <craigwhole@gmail.com>
2017-08-14 09:02:39 -0400

    width and height

--- Simulate.py
+++ Simulate.py
@@ -7,3 +7,12 @@ import random, sys, time, pygame
 from pygame.locals import *
 
 FPS = 30
+WINDOWWIDTH = 640
+WINDOWHEIGHT = 480
+FLASHSPEED = 500 #IN MS
+FLASHDELAY = 200 #IN MS
+BUTTONSIZE = 200
+BUTTONGAPSIZE = 20
+TIMEOUT = 4 #seconds before gameover if no button pressed
+
+# RGB
